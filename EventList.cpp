6cb3eff40811596069d4590479ef655b03fe0968
peterpashkin <pashkinpeter@gmail.com>
2024-02-06 22:09:16 +0100

    changed readme, deleted folder

---
+++ EventList.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Peter Pashkin on 04.12.23.
+//
+#include "EventList.h"
+
+EventList::EventList(uint32_t size) : events(size) {}
+
+void EventList::append(Event event) {
+    events.push_back(event);
+}
+
+void EventList::insert(Event event, int index) {
+    events[index] = event;
+}
+
+std::span<Event> EventList::get_events(uint32_t start_version, uint32_t end_version) {
+    std::span<Event> result(events.begin() + start_version, events.begin() + end_version);
+    return result;
+}
+
+
+void EventList::append_list(std::vector<Event> appending_events) {
+    this->events.insert(this->events.end(), appending_events.begin(), appending_events.end());
+}
+
---
+++ EventList.h
@@ -0,0 +1,50 @@
+//
+// Created by Peter Pashkin on 04.12.23.
+//
+
+// TODO do a parent class to cover EventList and DoubleEventList (i think generics are better)
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <span>
+#include <cstdint>
+
+#ifndef TIMELINEINDEX_EVENTLIST_H
+#define TIMELINEINDEX_EVENTLIST_H
+
+enum EventType {
+    INSERT,
+    DELETE
+};
+
+
+struct Event {
+    uint32_t row_id;
+    uint32_t row_id_second = -1;
+    EventType type;
+
+    // constructors as clang seems to not like implicit constructors
+    Event(uint32_t row_id, uint32_t row_id_second, EventType type) : row_id(row_id), row_id_second(row_id_second), type(type) {}
+    Event() = default;
+};
+
+/**
+ * @brief EventList class
+ * @details This class represents all the Events that happened since the start
+ * of the Timeline. It is a list of Events, sorted by their timestamp.
+ */
+class EventList {
+    friend class TimelineIndex;
+    std::vector<Event> events;
+
+public:
+    EventList() = default;
+    explicit EventList(uint32_t size);
+    void append(Event event);
+    std::span<Event> get_events(uint32_t start_version, uint32_t end_version);
+    void append_list(std::vector<Event> events);
+    void insert(Event event, int index);
+
+};
+
+#endif //TIMELINEINDEX_EVENTLIST_H
